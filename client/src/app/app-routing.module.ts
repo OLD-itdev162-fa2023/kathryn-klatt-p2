1846a99c98b08390289dcb11645414f7205ea3e8
Lint404 <klattkl@gmatc.matc.edu>
2022-11-16 15:50:02 -0600

    Added routes to app routing module.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ReadComponent } from './read/read.component';
+import { DeleteComponent } from './delete/delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component: HomepageComponent},
+{path:'character/:id', component: ReadComponent},
+{path:'create', component: CreateComponent},
+{path:'edit', component: EditComponent},
+{path:'delete', component: DeleteComponent},
+{path: '**', component: HomepageComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
