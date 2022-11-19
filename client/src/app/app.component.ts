fae2b81fb835b7c7393afe671f78601be17aa05c
Lint404 <klattkl@gmatc.matc.edu>
2022-11-19 13:51:42 -0600

    Test push.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  title = 'RPG Character Creation';
+  title = 'RPG Character Builder';
   Character: any;
   constructor(private http: HttpClient){}
 
