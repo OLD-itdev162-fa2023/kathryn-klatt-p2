f9a7bf92e940c7a06e6dcd5639c03dc7cd544ae0
Lint404 <klattkl@gmatc.matc.edu>
2022-11-21 22:33:28 -0600

    Updated character read (view) component.

--- client/src/app/read/read.component.ts
+++ client/src/app/read/read.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.css']
 })
-export class ReadComponent implements OnInit {
 
-  constructor() { }
+export class ReadComponent implements OnInit {
+  post: any = {}
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost(){
+    let id = this.route.snapshot.paramMap.get('id');
+    this.http.get(`http://localhost:5035/api/Character/${id}`).subscribe(post => {
+      this.post = post;
+    });
   }
 
 }
