60ca04dda7e36e75259d9df4a67ab89682f2a254
Lint404 <klattkl@gmatc.matc.edu>
2022-11-28 21:26:32 -0600

    Starting from a running page... but home page is still blank.

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -8,13 +8,18 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  posts: any;
+  characters: any;
+  charList = [];
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:5035/api/Character/GetAll').subscribe(
-      response => {this.posts = response; console.log(response)},
+      response => {
+        this.characters = response; //storing JSON response to character element
+        this.charList=this.characters.list; // array list created using JSON element property
+        console.log(this.charList)},
       error => {console.log(error)}
     );
   }
